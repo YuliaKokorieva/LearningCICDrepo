80f6c4e0feb19a4ca9cbcff11d28e544d83e3c09
YuliaKokorieva <yuliya.pastushenko@gmail.com>
2022-06-22 16:23:39 +0300

    pipeline setup

--- cypress/e2e/anecdotes.cy.js
+++ cypress/e2e/anecdotes.cy.js
@@ -1,7 +1,7 @@
 describe('Anecdotes', () => {
 
   it('front page can be opened', () => {
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:5000')
     cy.contains('Anecdotes')
     cy.contains('Debugging is twice')
   })
